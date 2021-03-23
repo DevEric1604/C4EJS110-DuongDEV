fc347415d5cabedc500ef8551006ca8fc53383f4
DevEric1604 <nguyeneric242@gmail.com>
2021-03-23 08:03:24 +0700

    sessionvideo

---
+++ Sesion3video/index.js
@@ -0,0 +1,109 @@
+console.log('Bài 1 ');
+// after 2 4 5
+// let movie = ['Jhon Wich', 'Peaky blinder', 'Amazing Spiderman '];
+
+// let newItem = prompt('Mời nhập tên phim :')
+// movie.push(newItem);
+// console.log(movie);
+
+
+//after 6 
+// let movie = ['Jhon Wich', 'Peaky blinder', 'Amazing Spiderman '];
+// let i = prompt('mời bạn nhập vị trí');
+// let newmovie = prompt(' mời nhập phim ');
+// movie[i] = newmovie;
+// console.log[i];
+// console.log(movie);
+
+
+// after 7 
+// let movie = ['Jhon Wich', 'Peaky blinder', 'Amazing Spiderman ', 'Attack on Titan ', 'Naruto', 'Indicious'];
+// let i = prompt(' mời nhập vị trí ');
+// let n = prompt(' mời nhập số lượng ');
+// movie.splice(i, n);
+// console.log(movie);
+
+//after 10 
+// let movie = ['Jhon Wich', 'Peaky blinder', 'Amazing Spiderman ', 'Attack on Titan ', 'Naruto', 'Indicious'];
+// for (let i = 0; i < movie.length / 2; i++) {
+//     console.log(movie[i]);
+// }
+// for (let i = 0; i < movie.length; i++) {
+//     console.log(movie[i]);
+// }
+
+// update all 11
+// let movie = ['Jhon Wich', 'Peaky blinder', 'Amazing Spiderman ', 'Attack on Titan ', 'Naruto', 'Indicious'];
+// for (let i = 0; i < movie.length; i++) {
+//     console.log(movie[i].toLowerCase());
+// }
+
+
+//Purpose of While
+// let x = prompt('Nhập Tên :  ');
+// if (x.length > 15) {
+//     console.log('the name is to long , please re-enter a new name');
+// } else {
+//     console.log('the good name ');
+// }
+
+// console.log(' How many leg does a spider have ');
+// let legspider = ['None', '4 legs', '8 legs', '12 legs'];
+// for (let i = 0; i < legspider.length; i++) {
+//     console.log(`${i+1}. ` + legspider[i])
+// }
+
+// let x = prompt('Enter Answer');
+// if (x < 1 || x > 4) {
+//     console.log('Invalide choice, the answer must be 1, 2, 3 or 4');
+// } else if (x != 3) {
+//     console.log('Good luck next time ');
+// } else {
+//     console.log(' Bravo , are you correct ');
+
+// }
+
+
+// bt nang cao 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Lí thuyết 
+// let menu = ['com', 'trung ran ', 'rau luoc ', 'bun'];
+// console.log(menu);
+// for (let i in menu) {
+//     console.log(menu[i]); // let in in biến trong index(i)
+// };
+// for(let i of menu ){
+//     console.log(x); // let of in biến trong giá trị mảng , không phải index .
+// };
+
+
+// // let newItem = 'Mien Ngu sac';
+
+// // menu[1] = 'Mien Ngu sac';
+// // console.log(menu);
+
+// // menu.splice(1, 2);
+// // console.log(menu);
+// // for (i = 0; i < menu.length; i++) {
+// //     menu[i] = menu[i].toUpperCase();
+// // };
+// // console.log(menu);
